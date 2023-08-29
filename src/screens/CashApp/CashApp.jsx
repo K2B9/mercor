6ea2c803da09f0e171e97f8bf00724c3f5387f7a
K2B9 <115083752+K2B9@users.noreply.github.com>
2023-08-29 19:58:52 +0530

    Initial commit

---
+++ src/screens/CashApp/CashApp.jsx
@@ -0,0 +1,425 @@
+import React from "react";
+import "./style.css";
+
+export const CashApp = () => {
+  return (
+    <div className="cash-app">
+      <div className="div">
+        <div className="overlap">
+          <div className="hero-section">
+            <div className="overlap-group">
+              <img
+                className="eye-button"
+                alt="Eye button"
+                src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/eye-button.svg"
+              />
+              <div className="navbar">
+                <div className="text-wrapper">SIGN IN</div>
+                <div className="text-wrapper">LEGAL</div>
+                <div className="licenses">LICENSES</div>
+                <div className="text-wrapper">SECURITY</div>
+                <div className="text-wrapper">CAREERS</div>
+                <div className="text-wrapper">PRESS</div>
+                <div className="text-wrapper">SUPPORT</div>
+                <div className="text-wrapper">STATUS</div>
+                <div className="text-wrapper">CODEBLOG</div>
+              </div>
+              <img
+                className="cashapp-logo"
+                alt="Cashapp logo"
+                src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/cashapp---logo.svg"
+              />
+              <div className="rays">
+                <div className="hero">
+                  <div className="overlap-group-2">
+                    <img
+                      className="layer"
+                      alt="Layer"
+                      src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/layer-1.png"
+                    />
+                    <div className="text-wrapper-2">CASH</div>
+                    <img
+                      className="intro-phone"
+                      alt="Intro phone"
+                      src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/intro-phone-1@2x.png"
+                    />
+                    <div className="APP">
+                      <br />
+                      APP
+                    </div>
+                    <img
+                      className="intro-cube"
+                      alt="Intro cube"
+                      src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/intro-cube-1@2x.png"
+                    />
+                    <img
+                      className="intro-cubes"
+                      alt="Intro cubes"
+                      src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/intro-cubes-1@2x.png"
+                    />
+                    <img
+                      className="intro-pillar"
+                      alt="Intro pillar"
+                      src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/intro-pillar-1@2x.png"
+                    />
+                    <img
+                      className="intro-stairs"
+                      alt="Intro stairs"
+                      src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/intro-stairs-1@2x.png"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="fixed-footer">
+                <div className="download-icons">
+                  <div className="buttons">
+                    <div className="app-store">
+                      <img
+                        className="vector"
+                        alt="Vector"
+                        src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/vector.svg"
+                      />
+                      <div className="label">APP STORE</div>
+                    </div>
+                    <div className="google-play">
+                      <img
+                        className="img"
+                        alt="Vector"
+                        src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/vector-1.svg"
+                      />
+                      <div className="label-2">GOOGLE PLAY</div>
+                    </div>
+                  </div>
+                </div>
+                <div className="policy-text">
+                  <p className="brokerage-services">
+                    Brokerage services by Cash App Investing LLC, member FINRA /
+                    SIPC. <br />
+                    See our BrokerCheck. Investing involves risk; you may lose
+                    money. Bitcoin trading offered by Cash App. Cash App
+                    Investing does not trade bitcoin and Cash App is not a
+                    member of FINRA or SIPC. Cash App facilitates banking
+                    services through Sutton Bank and Lincoln Savings Bank,
+                    Members FDIC.
+                  </p>
+                </div>
+                <img
+                  className="buttons-2"
+                  alt="Buttons"
+                  src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/buttons.svg"
+                />
+                <img
+                  className="vector-2"
+                  alt="Vector"
+                  src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/vector-2.svg"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="section-payments">
+            <div className="overlap-2">
+              <img
+                className="payments-floor"
+                alt="Payments floor"
+                src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/payments-floor-1.svg"
+              />
+              <div className="section-text">
+                <div className="text-wrapper-3">Payments</div>
+                <p className="send-and-receive">
+                  Send and receive money with anyone, donate&nbsp;&nbsp;to an
+                  important cause, or tip professionals. <br />
+                  Just enter a $cashtag, phone number, or <br />
+                  scan their&nbsp;&nbsp;QR code to pay.
+                </p>
+              </div>
+              <img
+                className="payments-phone"
+                alt="Payments phone"
+                src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/payments-phone-1.png"
+              />
+              <img
+                className="payments-column"
+                alt="Payments column"
+                src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/payments-column.png"
+              />
+              <img
+                className="payments-pillar"
+                alt="Payments pillar"
+                src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/payments-pillar-large@2x.png"
+              />
+              <img
+                className="payments-pillar-2"
+                alt="Payments pillar"
+                src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/payments-pillar-small@2x.png"
+              />
+              <img
+                className="payments-pillar-3"
+                alt="Payments pillar"
+                src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/payments-pillar-medium-3@2x.png"
+              />
+              <img
+                className="payments-pillar-4"
+                alt="Payments pillar"
+                src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/payments-pillar-medium-3@2x.png"
+              />
+              <img
+                className="payments-pillar-5"
+                alt="Payments pillar"
+                src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/payments-pillar-medium-2@2x.png"
+              />
+              <img
+                className="payments-pillar-6"
+                alt="Payments pillar"
+                src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/payments-pillar-medium-3@2x.png"
+              />
+              <img
+                className="payments-column-2"
+                alt="Payments column"
+                src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/payments-column-1.png"
+              />
+              <img
+                className="payments-column-3"
+                alt="Payments column"
+                src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/payments-column-2.png"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="section-banking">
+          <div className="overlap-3">
+            <div className="overlap-4">
+              <div className="overlap-5">
+                <img
+                  className="banking-cubes"
+                  alt="Banking cubes"
+                  src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/banking-cubes@2x.png"
+                />
+                <img
+                  className="banking-track"
+                  alt="Banking track"
+                  src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/banking-track-1@2x.png"
+                />
+                <img
+                  className="banking-phone"
+                  alt="Banking phone"
+                  src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/banking-phone@2x.png"
+                />
+                <img
+                  className="banking-ramp"
+                  alt="Banking ramp"
+                  src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/banking-ramp-1@2x.png"
+                />
+                <div className="section-text-2">
+                  <div className="text-wrapper-4">Banking</div>
+                  <p className="receive-your">
+                    Receive your paycheck, tax returns, and other direct
+                    deposits up to two days early using your Cash App routing{" "}
+                    <br />
+                    and account number.
+                  </p>
+                </div>
+              </div>
+              <img
+                className="banking-ramp-2"
+                alt="Banking ramp"
+                src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/banking-ramp-2@2x.png"
+              />
+              <img
+                className="banking-track-2"
+                alt="Banking track"
+                src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/banking-track-2@2x.png"
+              />
+              <img
+                className="banking-tube"
+                alt="Banking tube"
+                src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/banking-tube@2x.png"
+              />
+              <div className="overlap-6">
+                <img
+                  className="banking-hole"
+                  alt="Banking hole"
+                  src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/banking-hole@2x.png"
+                />
+                <img
+                  className="banking-column"
+                  alt="Banking column"
+                  src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/banking-column@2x.png"
+                />
+              </div>
+            </div>
+            <img
+              className="banking-monster"
+              alt="Banking monster"
+              src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/banking-monster@2x.png"
+            />
+            <img
+              className="banking-stairs"
+              alt="Banking stairs"
+              src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/banking-stairs-1@2x.png"
+            />
+            <img
+              className="banking-stairs-2"
+              alt="Banking stairs"
+              src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/banking-stairs-2@2x.png"
+            />
+            <img
+              className="banking-hole-2"
+              alt="Banking hole"
+              src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/banking-hole-1@2x.png"
+            />
+          </div>
+        </div>
+        <div className="section-cash-card">
+          <div className="overlap-7">
+            <img
+              className="boost-stairs"
+              alt="Boost stairs"
+              src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/boost-stairs-1.png"
+            />
+            <img
+              className="boost-stairs-2"
+              alt="Boost stairs"
+              src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/boost-stairs-2.png"
+            />
+            <img
+              className="boost-card"
+              alt="Boost card"
+              src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/boost-card@2x.png"
+            />
+            <img
+              className="boost-coffee"
+              alt="Boost coffee"
+              src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/boost-coffee@2x.png"
+            />
+            <img
+              className="boost-hand"
+              alt="Boost hand"
+              src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/boost-hand@2x.png"
+            />
+            <img
+              className="boost-phone"
+              alt="Boost phone"
+              src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/boost-phone.png"
+            />
+            <img
+              className="boost-shoe"
+              alt="Boost shoe"
+              src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/boost-shoe@2x.png"
+            />
+            <img
+              className="boost-burger"
+              alt="Boost burger"
+              src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/boost-burger@2x.png"
+            />
+            <div className="section-text-3">
+              <div className="cash-card-boost">
+                Cash Card
+                <br />
+                &amp; Boost
+              </div>
+              <p className="p">
+                The Cash Card is a free, customizable debit card that lets you
+                pay online and in stores. It’s the only way to get
+                Boosts—instant discounts that work at places where you want to
+                spend.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="section-investing">
+          <div className="overlap-8">
+            <div className="investing-floor">
+              <div className="investing-graph-wrapper">
+                <img
+                  className="investing-graph"
+                  alt="Investing graph"
+                  src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/investing-graph-3-1.png"
+                />
+              </div>
+            </div>
+            <img
+              className="investing-rays"
+              alt="Investing rays"
+              src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/investing-rays-1.svg"
+            />
+            <img
+              className="investing-graph-2"
+              alt="Investing graph"
+              src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/investing-graph-2.png"
+            />
+            <img
+              className="investing-graph-3"
+              alt="Investing graph"
+              src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/investing-graph-1.png"
+            />
+            <div className="section-text-4">
+              <div className="text-wrapper-5">Stocks</div>
+              <p className="text-wrapper-6">
+                Whether you’re an expert or just getting started, Cash App is
+                the fastest and most accessible way to invest in stocks. Start
+                now with as little as $1.
+              </p>
+            </div>
+            <div className="section-text-5">
+              <div className="text-wrapper-5">Bitcoin</div>
+              <p className="text-wrapper-7">
+                Cash App is the fastest way to convert dollars to bitcoin. From
+                your home screen, six taps are all it takes to stack sats, buy
+                an entire bitcoin, or just see what it’s all about.
+              </p>
+            </div>
+            <div className="text-wrapper-8">Investing</div>
+            <img
+              className="investing-stocks"
+              alt="Investing stocks"
+              src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/investing-stocks@2x.png"
+            />
+            <img
+              className="investing-bitcoin"
+              alt="Investing bitcoin"
+              src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/investing-bitcoin@2x.png"
+            />
+            <div className="fixed-footer-2">
+              <div className="download-icons">
+                <div className="buttons">
+                  <div className="app-store-2">
+                    <img
+                      className="vector"
+                      alt="Vector"
+                      src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/vector-4.svg"
+                    />
+                    <div className="label-3">APP STORE</div>
+                  </div>
+                  <div className="google-play-2">
+                    <img
+                      className="img"
+                      alt="Vector"
+                      src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/vector-3.svg"
+                    />
+                    <div className="label-4">GOOGLE PLAY</div>
+                  </div>
+                </div>
+              </div>
+              <div className="policy-text">
+                <p className="brokerage-services-2">
+                  Brokerage services by Cash App Investing LLC, member FINRA /
+                  SIPC. <br />
+                  See our BrokerCheck. Investing involves risk; you may lose
+                  money. Bitcoin trading offered by Cash App. Cash App Investing
+                  does not trade bitcoin and Cash App is not a member of FINRA
+                  or SIPC. Cash App facilitates banking services through Sutton
+                  Bank and Lincoln Savings Bank, Members FDIC.
+                </p>
+              </div>
+              <img
+                className="buttons-2"
+                alt="Buttons"
+                src="https://generation-sessions.s3.amazonaws.com/d4011a7f922bd2615330f2cdacf5663f/img/buttons-1.svg"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
